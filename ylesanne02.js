1caf0fe0a141519dc57cc9512fb5f4335ecb2c91
alarilaine <alarilaine@gmail.com>
2026-01-28 17:49:44 +0200

    javaaa1

--- ylesanne02.js
+++ ylesanne02.js
@@ -9,7 +9,24 @@ let sekund = 59;
 let aeg = tund+":" +minut+":"+sekund+"pm";
 console.log(aeg);
 
-let tsitaat = "To be, or not to be, that is the question. - William Shakespeare";
+const tsitaat = '"To be, or not to be, that is the question. - William Shakespeare"';
 
 
-console.log(tsitaat);
+let string=tsitaat;
+
+console.log(string);
+
+const perenimi="Jüri";
+const eesnimi="Jurakas";
+console.log("Jyri Jurakas nimetähed on: "+ perenimi.charAt(0) + "." + perenimi.charAt(0) + ".");
+
+let nimi="Perenimi,Eesnimi";
+console.log(nimi.split(",")[1]);
+console.log(nimi.split(",")[0].toUpperCase());
+console.log("Perenimi pikkus on " + nimi.split(",")[0].length + " tähte");
+
+let email="annamusi@gmail.com";
+console.log(email.replaceAll("gmail", "hotmail"));
+
+let andmerida="1,Marshal,Martinovic,mmartinovic0@dedecms.com,Male,40.19.226.175"
+console.log(andmerida.split(",")[5] + " ja " + andmerida.split("@")[0].split(",")[3]);
---
+++ ylesanne03.js
@@ -0,0 +1,28 @@
+// ylesanne02.js
+// Alari Laine
+// 28.01.2026
+
+//S6idu aeg ja kaugus
+let kaugus = 200; //km
+let kmh = 55;
+
+let aeg = kaugus / kmh;
+console.log("S6idu aeg on u " + Math.round(aeg) + " tundi.");
+
+//Postituse kuvamine
+
+let postitusi = 137;
+let postitusilehel = 10;
+
+let lehte = postitusi / postitusilehel;
+let viimaneleht = postitusi % postitusilehel;
+console.log("Lehti on " + Math.ceil(lehte) + " ja viimasel lehel " + viimaneleht + " postitust.");
+
+//Serveri tookulu
+
+let voimsus = 400; //vatti
+let elekterihind = 9.69 //senti kWh
+
+let voolutarb = voimsus / 1000; 
+let tookulu = voolutarb * elekterihind;
+console.log("Serveri tookulu on " + tookulu.toFixed(2) + " euri tunnis.");
