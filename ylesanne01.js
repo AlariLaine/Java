d6f6b9531bae055abeae789b99535248c8af3c78
alarilaine <alarilaine@gmail.com>
2026-01-28 09:09:32 +0200

    java

---
+++ ylesanne01.js
@@ -0,0 +1,27 @@
+// ylesanne01.js
+// Alari Laine
+// 27.01.2026
+
+const Number = 4.2;
+console.log(typeof Number); // Output: "number"
+
+const string = 'Will Code 4 food';
+console.log(typeof string); // Output: "string"
+
+const numb = "123";
+console.log(typeof numb); // Output: "boolean"
+
+const boolean = true;
+console.log(typeof boolean); // Output: "undefined"
+
+const arr = [1,2,3];
+console.log(typeof arr); // Output: "object"
+
+const w = undefined;
+console.log(typeof w);
+
+const strig = 'true';
+console.log(typeof strig); // Output: "string"   
+
+const bool = FALSE;
+console.log(typeof bool); // Output: "object"
---
+++ ylesanne02.js
@@ -0,0 +1,15 @@
+// ylesanne02.js
+// Alari Laine
+// 27.01.2026
+
+let tund = 2;
+let minut = 38;
+let sekund = 59;
+
+let aeg = tund+":" +minut+":"+sekund+"pm";
+console.log(aeg);
+
+let tsitaat = "To be, or not to be, that is the question. - William Shakespeare";
+
+
+console.log(tsitaat);
